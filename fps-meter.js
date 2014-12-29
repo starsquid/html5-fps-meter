e72d0c59f9c09b20def66f177051351576ac80ea
starsquid <starsquid@users.noreply.github.com>
2014-12-29 07:33:58 +0000

    Split monostate "fps" object into chart and block sub-objects
    Use frameTime "blocks", in prep for per-segment FPS calculations

--- fps-meter.js
+++ fps-meter.js
@@ -1,131 +1,242 @@
-var fps = {
-    frameStartTime : Date.now(),
-    frameInFrame   : false,
+// Lifecycle overview:
+// * HTML page includes this script file (and D3.js)
+// * Page calls createChart(), and adds the resulting floating <div> to the page
+// * Page calls installTimer(), which will redraw the chart on a JS timer
+// * HTML page adds a call to markNewFrame() whenever a new frame is drawn.
+//   Only pages that use window.requestAnimFrame() are supported.
+//
+function fps() {
+    // FPS chart is broken into blocks, representing 1 second of profiling info (by default)
+    // Each block stores data for frames that started within that second.
+    //
+    // These are data-only objects, whose rendering and lifecycle is managed by "fps"
+    // Time is measured in milliseconds, based on whatever Date.now() returns
+    //
+    // frameTimeData is strictly ordered, with these guarantees:
+    // * startTimes monotonically increasing
+    // * endTime cannot come before startTime for that pair
+    //
+    block = function() {
+        // each object in this array is a pair
+        //   startTime
+        //   endTime
+        //
+        // endTime tends to just be the value of startTime from the last frame
+        //
+        this.frameTimeData = [];
+    };
 
-    frameTimeData : [],
-    chartWidth    : 800,
-    chartHeight   : 120,
-    chartMaxY     : 200,
-    chartMinX     : -10 * 1000,
+    blockList = {
+        // list of blocks that no longer need processing
+        finishedBlocks: [],
+        // currentBlock must always exist
+        currentBlock: new block(),
 
-    frameTimeChart : null,
-    xAxis : null,
-    yAxis : null,
+        // blockDuration is minimum elapsed time before closing currentBlock
+        blockDuration: 1000,
 
+
+        clearData: function() {
+            this.finishedBlocks = []
+            this.currentBlock = new block()
+        },
+
+        getAllData: function() {
+            var allData = []
+            this.finishedBlocks.forEach(
+                function(d) { allData = allData.concat(d.frameTimeData) } )
+
+            return allData.concat(this.currentBlock.frameTimeData)
+        },
+
+        getAllDataBounded: function() {
+            var allDataBounded = []
+
+            this.getAllData().forEach(
+                function(d) {
+                    allDataBounded.push({xcoord: d.startTime, ycoord: d.endTime - d.startTime});
+                    allDataBounded.push({xcoord: d.endTime,   ycoord: d.endTime - d.startTime});
+                } )
+
+            return allDataBounded
+        },
+
+        addFrame: function(startTime, endTime) {
+            // add frame to end of block
+            this.currentBlock.frameTimeData.push({
+                startTime: startTime,
+                endTime:   endTime
+            })
+
+            // check if block is done
+            if (endTime - this.currentBlock.frameTimeData[0].startTime > this.blockDuration) {
+                this.finishedBlocks.push(this.currentBlock)
+                this.currentBlock = new block
+            }
+        },
+
+        shrinkBlocks: function(maxBlocks) {
+            while (this.finishedBlocks.length > maxBlocks)
+                this.finishedBlocks.shift()
+        }
+    };
+
+
+    // functions and prototypes related to capturing frame data
+    //-----------------------------------------------------------------
+    // currentFrameStartTime is only valid if currentlyMidFrame is true
+    this.currentlyMidFrame = false
+    this.currentFrameStartTime = null
+
+
+    // rendering and styling of frame data chart
+    // * Implemented as an SVG graphic on an HTML5 <div>
+    // * SVG data is manipulated using D3.js
+    //-----------------------------------------------------------------
+    chart = {
+        width:  800,
+        height: 120,
+
+        divContainer: null,
+        svg: null,
+        svg_y: null,
+
+        createDivContainer: function() {
+            this.divContainer = document.createElement('div')
+            this.divContainer.class = "fpsChart"
+            this.divContainer.setAttribute("style",
+                "position: absolute; " +
+                "z-index: 2000; " +
+                "background: black; " +
+                "width: " + this.width + "; " +
+                "height: " + this.height + "; " +
+                "")
+
+            return this.divContainer
+        },
+
+        createSvgLine: function() {
+            // create and add data to the chart
+            this.svg = d3.select(this.divContainer)
+                .append('svg')
+                .attr("width", ''+chart.width+'px')
+                .attr("height", ''+chart.height+'px')
+
+            // add empty initial line
+            var vis = d3.select(this.divContainer).selectAll('svg')
+            vis.selectAll('path.line').data([[]])
+                .enter().append("path")
+                .attr("d", d3.svg.line())
+                .attr("style", "fill: none")
+
+            // add y-axis
+            this.svg_y = this.svg.append("g")
+        },
+
+        refresh: function() {
+            // collate existing data together
+            // TODO: render each line/section independently?
+            var completeFrameTimeData = blockList.getAllDataBounded()
+
+            // pre-compute maximum frame time, so we can scale the chart's Y-axis
+            yAxisMax = Math.max.apply(null,
+                    completeFrameTimeData.map(function(d) { return d.endTime - d.startTime; } ))
+
+            // X-axis scaling: from 10 seconds ago to now
+            xScale = d3.scale.linear()
+                .domain([Date.now() - 10 * 1000, Date.now()])
+                .range([0, chart.width - 40]);
+            // Y-axis scaling: from 0 to max frame time * 1.2
+            yScale = d3.scale.linear()
+                .domain([0, 70])
+                .range([chart.height, 0]);
+
+            dataLine = d3.svg.line()
+                .x( function(d) { return xScale(d.xcoord); } )
+                .y( function(d) { return yScale(1000/(d.ycoord)); } )
+
+            // add data (plotline)
+            vis = d3.select(this.divContainer).selectAll('svg')
+            var newData = vis.selectAll('path')
+                .data([completeFrameTimeData])
+            newData
+                .attr("d", dataLine)
+                .attr("stroke", "#0f0")
+                .attr("stroke-width", "1px")
+                .exit().remove()
+
+            // add axis scaling
+            var yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .tickValues([30, 60])
+            this.svg_y.call(yAxis)
+                .attr("style", "stroke: white; fill: white; font-family: Avenir, Arial, arial, sans-serif")
+                .attr("transform", "translate(800,0)")
+        }
+    }
+
+    // chart is redrawn using a JavaScript timer
+    //-----------------------------------------------------------------
+    // redraw interval is in milliseconds
+    this.chartRedrawTimerInterval = 10
+    this.chartRedrawTimerID = null
+}
+
+fps.prototype = {
+    // functions and prototypes related to capturing frame data
+    //-----------------------------------------------------------------
     clearFrameTimeData: function() {
-        fps.frameStartTime = Date.now()
-        fps.frameInFrame   = false
-        fps.frameTimeData  = [
-            {timestamp: Date.now() + fps.chartMinX, frameTime: 1e6},
-            {timestamp: Date.now(),                 frameTime: 1e6}
-        ]
+        this.currentlyMidFrame = false
+        this.currentFrameStartTime = null
+
+        blockList.clearData()
     },
     markEndOfFrames: function() {
-        fps.frameInFrame = false
+        this.currentlyMidFrame = false
+        this.currentFrameStartTime = null
     },
 
-    createFrameTimeChart: function() {
-        fps.frameTimeChart = document.createElement('div')
-        fps.frameTimeChart.id = "frameTimeChart"
-        fps.frameTimeChart.setAttribute("style",
-            "position: absolute; " +
-            "z-index: 2000; " +
-            "padding: 2px; " +
-            "background: #000; " +
-            "width: " + fps.chartWidth + "; " +
-            "height: " + fps.chartHeight + "; ");
-
-        // create and add data to the chart
-        vis = d3.select(fps.frameTimeChart)
-            .append('svg:svg')
-            .attr("width", ''+fps.chartWidth+'px')
-            .attr("height", ''+fps.chartHeight+'px')
-
-        // add initial, uncalibrated plotline
-        vis = d3.select(fps.frameTimeChart).selectAll('svg')
-        vis.selectAll('path.line').data([fps.frameTimeData])
-            .enter().append("svg:path")
-            .attr("d", d3.svg.line())
-
-        fps.redrawChart()
-
-        return fps.frameTimeChart
-    },
     markNewFrame: function() {
-        if (fps.frameInFrame) {
-            frameEndTime = Date.now()
+        // if we were in the middle of a frame, add it
+        if (this.currentlyMidFrame) {
+            var currentFrameEndTime = Date.now()
 
-            if (fps.frameTimeData.length > 500)
-                fps.frameTimeData.shift()
-
-            fps.frameTimeData.push(
-                {timestamp: frameEndTime, frameTime: frameEndTime - fps.frameStartTime}
-            )
+            // add entries to list (new style)
+            blockList.addFrame(this.currentFrameStartTime, currentFrameEndTime)
         }
 
-        fps.frameInFrame = true
-        fps.frameStartTime = Date.now()
+        this.currentlyMidFrame = true
+        this.currentFrameStartTime = Date.now()
+
+        // remove old entries, as needed
+        blockList.shrinkBlocks(10)
+    },
+
+
+    // rendering and styling of frame data chart
+    //-----------------------------------------------------------------
+    createFrameTimeChart: function() {
+        container = chart.createDivContainer()
+        chart.createSvgLine()
+        chart.refresh()
+
+        return container
     },
     redrawChart: function() {
-        vis = d3.select(fps.frameTimeChart).selectAll('svg')
-
-        // pre-compute maximum frame time,
-        // so we can set it as the Y-axis max
-        yAxisMax = Math.max.apply(null,
-                fps.frameTimeData.map(function(d) { return d.frameTime; } ))
-
-        // set axis scaling for chart
-        xAxis = d3.scale.linear()
-            .domain([Date.now() + fps.chartMinX, Date.now()])
-            .range([0, fps.chartWidth]);
-        yAxis = d3.scale.linear()
-            .domain([0, yAxisMax * 1.2])
-            .range([fps.chartHeight, 0]);
-
-        dataLine = d3.svg.line()
-            .x( function(d) { return xAxis(d.timestamp); } )
-            .y( function(d) { return yAxis(d.frameTime); } )
-
-        // add data (plotline)
-        vis.selectAll('path')
-            .data([fps.frameTimeData])
-            .attr("d", dataLine)
-            .attr("stroke", "#0f0")
-            .attr("stroke-width", "3px")
+        chart.refresh()
     },
 
-    timerId: 0,
-    updateInterval: 30,
+    // chart is redrawn using a JavaScript timer
+    //-----------------------------------------------------------------
     installIntervalTimer: function() {
-        timerId = setInterval(function() {
-            fps.redrawChart()
-        }, fps.updateInterval);
+        var t = this
+        this.chartRedrawTimerID = setInterval(
+            function() {t.redrawChart()},
+            this.chartRedrawTimerInterval)
     },
     removeIntervalTimer: function() {
-        clearInterval(timerId)
-        timerId = 0
+        clearInterval(this.chartRedrawTimerID)
+        this.chartRedrawTimerID = null
     }
-}
-
-// example usage
-// -------------
-
-function __ignore_fps_init_sample() {
-    oldLoadFunction = window.onload
-    awindow.onload = function() {
-        if (oldLoadFunction)
-            oldLoadFunction()
-
-        fps.clearFrameTimeData()
-        document.body.appendChild(fps.createFrameTimeChart())
-        fps.installIntervalTimer()
-
-        mainLoop()
-    }
-}
-
-function __ignore_fps_mainLoop_sample() {
-    while (true) {
-        fps.markNewFrame();
-    }
-}
+};
